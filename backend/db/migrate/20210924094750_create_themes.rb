b564fc2dbb83c432cdae7edc5e998599b64e8c9c
hase <hase.kyoto423@gmail.com>
2021-09-24 20:44:42 +0900

    fix: type of foreign_key int to references

--- backend/db/migrate/20210924094750_create_themes.rb
+++ backend/db/migrate/20210924094750_create_themes.rb
@@ -1,10 +1,10 @@
 class CreateThemes < ActiveRecord::Migration[6.1]
   def change
     create_table :themes do |t|
-      t.int :user_id
+      t.references :user
       t.string :name
-      t.int :rooms_num
-      t.int :close_time
+      t.integer :rooms_num
+      t.integer :close_time
       t.bool :is_closed
 
       t.timestamps
