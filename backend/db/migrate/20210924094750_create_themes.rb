b16e589a4ed844d51f910878d06a8d7c44620ce8
hase <hase.kyoto423@gmail.com>
2021-09-24 18:48:39 +0900

    add: generate theme model

---
+++ backend/db/migrate/20210924094750_create_themes.rb
@@ -0,0 +1,13 @@
+class CreateThemes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :themes do |t|
+      t.int :user_id
+      t.string :name
+      t.int :rooms_num
+      t.datetime :close_time
+      t.bool :is_closed
+
+      t.timestamps
+    end
+  end
+end
