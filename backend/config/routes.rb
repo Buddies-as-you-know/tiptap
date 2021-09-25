854545a6aeb1458d10e836dc4cff401a0ae39df5
hase <hase.kyoto423@gmail.com>
2021-09-25 11:54:17 +0900

    fix: name of model user to users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :user, only: [:create, :show]
+    resources :users, only: [:create, :show]
     resources :user_taps, only: [:create]
   end
 end
