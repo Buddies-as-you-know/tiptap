78d8e542fd90da2f75f4f5657c761e8fd5921a76
junior <bluehawaiiouhu@gmail.com>
2021-09-25 11:17:45 +0900

    add default value 0 to count attr

--- backend/db/migrate/20210924093953_create_user_taps.rb
+++ backend/db/migrate/20210924093953_create_user_taps.rb
@@ -3,7 +3,7 @@ class CreateUserTaps < ActiveRecord::Migration[6.1]
     create_table :user_taps do |t|
       t.references :user
       t.references :room
-      t.integer :count, null: false
+      t.integer :count, null: false, default: 0
 
       t.timestamps
     end
