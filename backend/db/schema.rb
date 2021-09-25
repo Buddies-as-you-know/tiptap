f9b371d6d4cfb7b07fb66bb65ea4041bcefbbab5
junior <bluehawaiiouhu@gmail.com>
2021-09-25 13:09:11 +0900

    apply migration for typo

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_101537) do
   create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "theme_id"
     t.string "name"
-    t.integer "count", default: 0, null: false
+    t.integer "counts", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["theme_id"], name: "index_rooms_on_theme_id"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_101537) do
   create_table "user_taps", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "room_id"
-    t.integer "count", default: 0, null: false
+    t.integer "counts", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_user_taps_on_room_id"
