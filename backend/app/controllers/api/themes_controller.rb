9102de5092bbaf362c44ec255d638d014fb65f0f
hase <hase.kyoto423@gmail.com>
2021-09-25 17:21:17 +0900

    add: create room api

--- backend/app/controllers/api/themes_controller.rb
+++ backend/app/controllers/api/themes_controller.rb
@@ -10,5 +10,16 @@ class Api::ThemesController < ApplicationController
   end
 
   def create
+    @theme = Theme.create(theme_params)
+    @rooms = params[:rooms]
+    @rooms.each do |room|
+      Room.create({theme_id: @theme.id, name: room[:name]})
+    end
+  end
+
+  private
+
+  def theme_params
+    params.permit(:user_id, :name, :rooms_num, :close_time)
   end
 end
