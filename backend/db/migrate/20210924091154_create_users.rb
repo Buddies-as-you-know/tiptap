f68064d700933b5eb14c002ecdd18dae7b2d5719
junior <bluehawaiiouhu@gmail.com>
2021-09-25 11:15:49 +0900

    add association and validation to user model

--- backend/db/migrate/20210924091154_create_users.rb
+++ backend/db/migrate/20210924091154_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.integer :counts, null: false, default: 0
 
       t.timestamps
     end
