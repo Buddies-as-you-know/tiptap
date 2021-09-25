5953822e2b91c2fc00f3a143bea676b2fa6778d9
junior <bluehawaiiouhu@gmail.com>
2021-09-25 12:07:17 +0900

    create theme controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:create, :show]
     resources :user_taps, only: [:create]
+    resources :themes, only: [:index, :show, :create]
   end
 end
