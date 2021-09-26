b3692a39dc43c516069ba53daea8a4dd623ee355
hase <hase.kyoto423@gmail.com>
2021-09-26 22:43:16 +0900

    add: db:migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_101537) do
+ActiveRecord::Schema.define(version: 2021_09_26_133419) do
 
   create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "theme_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_101537) do
     t.integer "counts", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
