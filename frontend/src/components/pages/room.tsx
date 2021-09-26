f6ab0bc665f53bfed01839d39109bf0db4ccd5b9
H0R15H0 <shohori13@gmail.com>
2021-09-27 00:21:14 +0900

    Change: room page to switch template by closing time

--- frontend/src/components/pages/room.tsx
+++ frontend/src/components/pages/room.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react'
 
+import { theme_with_single_room_closed, theme_with_two_room_closed } from "../../mocks/mockData";
+import RoomResultTemplate from '../templates/roomResultTemplate'
 import RoomTemplate from '../templates/roomTemplate'
 
 const Room: FC = () => {
-   return <RoomTemplate />
+   return (
+   <>
+   {theme_with_single_room_closed.is_closed
+      ? <RoomResultTemplate theme_result={ theme_with_two_room_closed }/>
+      : <RoomTemplate/>
+   }
+   </>
+   )
 }
 
 export default Room
