f939ffa50ae810fafe05e670095279aabbbe1327
hase <hase.kyoto423@gmail.com>
2021-09-27 22:58:26 +0900

    fix: theme#create get user_id from current_api_user

--- backend/app/controllers/api/themes_controller.rb
+++ backend/app/controllers/api/themes_controller.rb
@@ -11,7 +11,8 @@ class Api::ThemesController < ApplicationController
   end
 
   def create
-    @theme = Theme.create(theme_params)
+    @theme = Theme.create(theme_params.except(:duration))
+    @theme.update(close_time: @theme.created_at.to_i + params[:duration].to_i)
     params[:rooms].each do |room_param|
       Room.create({theme_id: @theme.id, name: room_param[:name]})
     end
@@ -22,6 +23,6 @@ class Api::ThemesController < ApplicationController
   private
 
   def theme_params
-    params.permit(:name, :rooms_num, :close_time).merge(user_id: current_api_user.id)
+    params.permit(:name, :rooms_num,:duration).merge(user_id: current_api_user.id)
   end
 end
