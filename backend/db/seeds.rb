82d9a0885fae617054bbf5f7055abb8dd496604e
hase22 <43087984+hase22@users.noreply.github.com>
2021-09-27 14:44:02 +0900

    delete comment out

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,20 +1,5 @@
 require 'csv'
 
-# User.create({name: "otaka", email: "otaka@fuga.com", counts: 0})
-# User.create({name: "hosoisan", email: "hosoisan@fuga.com", counts: 0})
-# User.create({name: "junior", email: "junior@fuga.com", counts: 0})
-# Theme.create({ user_id: 1, name: 'おたかvsほそいさん', rooms_num: 2, close_time: 1632550340, is_closed: false})
-# Theme.create({ user_id: 2, name: 'ジュニア', rooms_num: 1, close_time: 1632550350, is_closed: false})
-# Room.create({theme_id: 1, name:"おたか"})
-# Room.create({theme_id: 1, name:"ほそいさん"})
-# Room.create({theme_id: 2, name:"junior"})
-# UserTap.create({user_id: 3, room_id:1, counts: 10})
-# UserTap.create({user_id: 3, room_id:1, counts: 15})
-# UserTap.create({user_id: 3, room_id:2, counts: 10})
-# UserTap.create({user_id: 3, room_id:2, counts: 10})
-# UserTap.create({user_id: 3, room_id:2, counts: 10})
-# UserTap.create({user_id: 1, room_id:3, counts: 25})
-
 ActiveRecord::Base.transaction do
   CSV.foreach('db/users.csv') do |row|
     User.create(
