5cdebb9a561f33b1ae52475b3b34e25f43a46ab6
junior <bluehawaiiouhu@gmail.com>
2021-09-27 11:44:36 +0900

    [backend] implement auth function

---
+++ backend/app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,6 @@
+class Api::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+  def sign_up_params
+    params.require(:registration).permit(:name, :email, :password)
+  end
+end
