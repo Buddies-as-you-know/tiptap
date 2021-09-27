1528e529eed7fef03aafbddc752c988a6659b060
hase <hase.kyoto423@gmail.com>
2021-09-28 04:18:41 +0900

    add: theme#show

--- backend/app/controllers/api/themes_controller.rb
+++ backend/app/controllers/api/themes_controller.rb
@@ -1,5 +1,5 @@
 class Api::ThemesController < ApplicationController
-  before_action :authenticate_api_user!, only: [:create]
+  before_action :authenticate_api_user!, only: [:create, :show]
   def index
     sorted_theme_ids = Room.order('sum_counts desc').group(:theme_id).sum(:counts).keys
     @themes = Theme.find(sorted_theme_ids)
@@ -10,6 +10,16 @@ class Api::ThemesController < ApplicationController
   end
 
   def show
+    @theme = Theme.find(params[:id])
+    @rooms = @theme.rooms
+    @rooms.each do |room|
+      #user_room_total_tapsはroom attr_accessorに持たせたい
+      @user_room_total_taps = UserTap.where(user_id: 1, room_id: 1).sum(:counts)
+      # room.user_room_total_taps = UserTap.find_by(user_id: current_api_user.id, room_id: room.id).sum(:counts)
+    end
+    # if Time.now < @theme.close_time
+    #   @rooms = @theme.rooms
+    render "api/theme/show.json.jb"
   end
 
   def create
@@ -25,6 +35,7 @@ class Api::ThemesController < ApplicationController
   private
 
   def theme_params
-    params.permit(:name, :rooms_num,:duration).merge(user_id: current_api_user.id)
+    params.permit(:name, :rooms_num, :duration).merge(user_id: current_api_user.id)
   end
+
 end
